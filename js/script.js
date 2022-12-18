acc6ae82176998211177e9d82883a30491ba09b4
Kjell-Arne Neshagen Mar22 FT <kjell-arne@neshagen.no>
2022-12-18 22:19:52 +0100

    changed footer behavior to follow content. changed bg color on body after customers request

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+const form = document.getElementById("contact-form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("e-mail");
+const subjectInput = document.getElementById("subject");
+const messageInput = document.getElementById("message");
+
 document.getElementById('hamburger-menu').addEventListener('click', () => {
   document.getElementById('mobile-links-menu').style.display = 'flex';
 })
