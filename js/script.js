e96c7678be7394ccbbc2ad64fe9ceed0cc2d6b64
Kjell-Arne Neshagen Mar22 FT <kjell-arne@neshagen.no>
2022-12-18 23:36:07 +0100

    Finished validation, and cleaned up code

--- js/script.js
+++ js/script.js
@@ -3,14 +3,13 @@ const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("e-mail");
 const subjectInput = document.getElementById("subject");
 const messageInput = document.getElementById("message");
-let error = [];
+const errorSections = document.querySelector(".errorSection");
+
 
 const validMail = (address) => {
   return String(address)
     .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+    .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 };
 
 document.getElementById('hamburger-menu').addEventListener('click', () => {
@@ -18,24 +17,30 @@ document.getElementById('hamburger-menu').addEventListener('click', () => {
 });
 
 form.addEventListener("submit", (e) => {
+  let error = [];
   e.preventDefault();
-  if (nameInput.length <= 5) {
+  if (nameInput.value.length <= 5) {
     error.push("Please provide a Name of 5 characters or more");
   }
-  if (!validMail(emailInput)) {
+  if (!validMail(emailInput.value)) {
+    console.log(validMail(emailInput.value));
     error.push("Please provide an valid e-mail address");
   }
-  if (subjectInput.length <= 5) {
+  if (subjectInput.value.length <= 5) {
     error.push("Please provide an Subject of 5 characters or more");
   }
-  if (messageInput.length < 20 || messageInput.length > 100) {
+  if (messageInput.value.length < 20 || messageInput.length > 100) {
     error.push("Your message must be more then 20 characters, and less then 100 characters");
   }
 
-  if(error.length > 0) {
-
+  if(error.length < 1) {
+    window.location.href = "contactrecived.html";
   } else {
-
+    let infoElem = "";
+    error.forEach(err => {
+      infoElem += `<p class="error">${err}</p>`;
+    });
+    errorSections.innerHTML = infoElem;
   }
 });
 
